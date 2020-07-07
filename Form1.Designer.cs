4ab8cd29909bb0bb4bd6a39d19ca0b4ff02352f4
iamverylovely <sa@iamverylovely.com>
2020-07-07 14:17:37 +0800

    debug

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace MouseClickTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.is_ms = new System.Windows.Forms.TextBox();
+            this.is_begin = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "说明";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(328, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(383, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "https://github.com/iamverylovely/MouseClickTool";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "源代码：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "本软件为开源软件，感谢支持！";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(775, 96);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "使用帮助";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(762, 60);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "设置好点击频率，点击开始，将鼠标悬浮在需要点击的区域上方，5秒后开始自动点击。";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.is_ms);
+            this.groupBox3.Controls.Add(this.is_begin);
+            this.groupBox3.Location = new System.Drawing.Point(12, 245);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(776, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "功能";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(21, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "频率(毫秒)：";
+            // 
+            // is_ms
+            // 
+            this.is_ms.Location = new System.Drawing.Point(125, 47);
+            this.is_ms.MaxLength = 10;
+            this.is_ms.Name = "is_ms";
+            this.is_ms.Size = new System.Drawing.Size(100, 25);
+            this.is_ms.TabIndex = 1;
+            this.is_ms.Text = "1000";
+            // 
+            // is_begin
+            // 
+            this.is_begin.Location = new System.Drawing.Point(686, 42);
+            this.is_begin.Name = "is_begin";
+            this.is_begin.Size = new System.Drawing.Size(75, 30);
+            this.is_begin.TabIndex = 0;
+            this.is_begin.Text = "开始";
+            this.is_begin.UseVisualStyleBackColor = true;
+            this.is_begin.Click += new System.EventHandler(this.is_begin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 365);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "鼠标连点器";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox is_ms;
+        private System.Windows.Forms.Button is_begin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseClickTool
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
+
+        // private const uint RightDown = 0x0008;
+        // private const uint RightUp = 0x0010;
+        private const uint LeftDown = 0x0002;
+        private const uint LeftUp = 0x0004;
+
+        public static void SendRightClick(uint posX, uint posY)
+        {
+            mouse_event(LeftDown, posX, posY, 0, new System.IntPtr());
+            mouse_event(LeftUp, posX, posY, 0, new System.IntPtr());
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void is_begin_Click(object sender, EventArgs e)
+        {
+            if (is_begin.Text == "停止")
+            {
+                Environment.Exit(0);
+                return;
+            }
+            string ms = is_ms.Text;
+            int result;
+            if (int.TryParse(ms, out result) && result > 0)
+            {
+                is_ms.ReadOnly = true;
+                Task.Factory.StartNew(async () =>
+                 {
+                     await Task.Run(() =>
+                     {
+                         for (int i = 1; i < 5; i++)
+                         {
+                             is_begin.Text = string.Format("开始({0})", 5 - i);
+                             Thread.Sleep(1000);
+                         }
+                     });
+                     is_begin.Text = string.Format("停止");
+                     for (; ; )
+                     {
+                         await Task.Run(() =>
+                         {
+                             uint x = (uint)Cursor.Position.X;
+                             uint y = (uint)Cursor.Position.Y;
+                             SendRightClick(x, y);
+                             Thread.Sleep(result);
+                         });
+                     }
+
+                 });
+            }
+            else
+            {
+                MessageBox.Show("输入的数字不正确，必须是正整数");
+            }
+        }
+    }
+}
