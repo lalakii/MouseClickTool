59a58e6d9ec93935371ef05f9624597948a62c75
lalakii <i@lalaki.cn>
2024-07-20 16:35:28 +0800

    :white_check_mark: v1.72 clean

---
+++ MouseClickTool.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+[assembly: AssemblyVersion("1.7.0.2")]
+[assembly: AssemblyFileVersion("1.7.0.2")]
+[assembly: AssemblyTitle("MouseClickTool")]
+[assembly: AssemblyProduct("MouseClickTool")]
+[assembly: AssemblyCopyright("Copyright (C) 2024 lalaki.cn")]
+
+namespace def
+{   /// <summary>
+    /// 怎么简单怎么来了
+    /// </summary>
+    public partial class MouseClickTool : Form
+    {
+        private Input input;
+        private bool running = false;
+        private const int waitSeconds = 3;
+        private const int hotkeyId = 0x233;
+        private readonly string[] config = { "F1", "1000", "0" };
+
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
+        [DllImport("shell32.dll")]
+        private static extern int ShellExecute(int hwnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, int nShowCmd);
+
+        //参考：https://stackoverflow.com/questions/5094398/how-to-programmatically-mouse-move-click-right-click-and-keypress-etc-in-winfo
+        [DllImport("user32.dll")]
+        private static extern uint SendInput(uint nInputs, ref Input pInputs, int cbSize);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Input
+        {
+            public int type;
+            public MouseKeybdhardwareInputUnion mkhi;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct MouseKeybdhardwareInputUnion
+        {
+            [FieldOffset(0)]
+            public MouseInput mi;
+        }
+
+        [Flags]
+        private enum MouseEventFlag : uint
+        {
+            MOUSEEVENTF_LEFTDOWN = 0x0002,
+            MOUSEEVENTF_LEFTUP = 0x0004,
+            MOUSEEVENTF_RIGHTDOWN = 0x0008,
+            MOUSEEVENTF_RIGHTUP = 0x0010,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MouseInput
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public MouseEventFlag dwFlags;
+            public uint time;
+            public unsafe ulong* dwExtraInfo;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && (int)m.WParam == hotkeyId)
+            {
+                btnStart.PerformClick();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void UpdateBtnStartText()
+        {
+            btnStart.Text = (running ? "停止" : "开始") + "(" + config[0] + ")";
+        }
+
+        private MouseClickTool()
+        {
+            InitializeComponent();
+            var configFile = Path.Combine(Path.GetTempPath(), "lalaki_mouse_click_tool.ini");
+            if (File.Exists(configFile))
+            {
+                var tempConfig = File.ReadAllLines(configFile);
+                if (tempConfig.Length > 2)
+                {
+                    config = tempConfig;
+                }
+            }
+            for (int i = 1; i < 13; i++)
+            {
+                hotkeys.Items.Add("F" + i);
+            }
+            hotkeys.SelectedIndexChanged += (_, __) =>
+             {
+                 UnregisterHotKey(Handle, hotkeyId);
+                 Enum.TryParse(hotkeys.Text, out Keys key);
+                 RegisterHotKey(Handle, hotkeyId, 0x4000, (uint)key);
+                 var keyStr = key.ToString();
+                 config[0] = keyStr;
+                 UpdateBtnStartText();
+             };
+            delayVal.TextChanged += (_, __) => config[1] = delayVal.Text;
+            clickType.SelectedIndexChanged += (_, __) => config[2] = clickType.SelectedIndex == 0 ? "0" : "1";
+            hotkeys.SelectedItem = config[0];
+            delayVal.Text = config[1];
+            int.TryParse(config[2], out int clickTypeIndex);
+            clickType.SelectedIndex = clickTypeIndex;
+            btnUrl.Click += (__, _) => ShellExecute(0, "open", btnUrl.Text, "", "", 1);
+            btnClose.Click += (__, _) =>
+            {
+                Hide();
+                running = false;
+                Application.Exit();
+            };
+            FormClosing += (__, _) => File.WriteAllLines(configFile, config);
+            btnMin.Click += (__, _) => WindowState = FormWindowState.Minimized;
+            btnClose.MouseHover += (__, _) => btnClose.ForeColor = Color.IndianRed;
+            btnClose.MouseLeave += (__, _) => btnClose.ForeColor = Control.DefaultForeColor;
+            btnMin.MouseHover += (__, _) => btnMin.ForeColor = Color.DodgerBlue;
+            btnMin.MouseLeave += (__, _) => btnMin.ForeColor = Control.DefaultForeColor;
+            Resize += (_, __) => WindowState = WindowState == FormWindowState.Maximized ? FormWindowState.Normal : WindowState;
+            MouseUp += (_, __) => Cursor = Cursors.Default;
+            MouseDown += (_, e) => Tag = (e.Button == MouseButtons.Left) ? (object)e.Location : null;
+            MouseMove += (_, e) =>
+            {
+                if (e.Button == MouseButtons.Left && Tag is Point offset)
+                {
+                    Cursor = Cursors.SizeAll;
+                    var location = PointToScreen(e.Location);
+                    location.Offset(-offset.X, -offset.Y);
+                    Location = location;
+                }
+            };
+            Paint += (_, e) =>
+            {
+                var g = e.Graphics;
+                using (var pen = new Pen(Color.MediumPurple, 7f))
+                    g.DrawLine(pen, Width, 0, 0, 0);
+                using (var border = new Pen(Color.FromArgb(60, 0, 0, 0)))
+                    g.DrawRectangle(border, 0, 0, Width - 1, Height - 1);
+                g.DrawString(Text, new Font("Consolas", 16f), Brushes.DimGray, 15, 20);
+            };
+            btnStart.Click += (__, _) =>
+            {
+                if (!running && clickType.Enabled)
+                {
+                    if (int.TryParse(delayVal.Text, out int delay) && delay > -1)
+                    {
+                        delayVal.ReadOnly = running = true;
+                        btnStart.Enabled = clickType.Enabled = false;
+                        Task.Run(async () =>
+                        {
+                            await Task.Run(async () =>
+                            {
+                                for (int i = 1; i < waitSeconds; i++)
+                                {
+                                    btnStart.Text = string.Format("{0}", waitSeconds - i);
+                                    await Task.Delay(1000);
+                                }
+                            });
+                            btnStart.Enabled = true;
+                            UpdateBtnStartText();
+                            var downFlag = MouseEventFlag.MOUSEEVENTF_LEFTDOWN;
+                            var upFlag = MouseEventFlag.MOUSEEVENTF_LEFTUP;
+                            if (clickType.SelectedIndex == 1)
+                            {
+                                downFlag = MouseEventFlag.MOUSEEVENTF_RIGHTDOWN;
+                                upFlag = MouseEventFlag.MOUSEEVENTF_RIGHTUP;
+                            }
+                            var size = Marshal.SizeOf(input);
+                            while (running)
+                            {
+                                await Task.Run(async () =>
+                                {
+                                    input.mkhi.mi.dwFlags = downFlag;
+                                    SendInput(1, ref input, size);
+                                    input.mkhi.mi.dwFlags = upFlag;
+                                    SendInput(1, ref input, size);
+                                    if (delay != 0)
+                                    {
+                                        await Task.Delay(delay);
+                                    }
+                                });
+                            }
+                            if (delay == 0)
+                            {
+                                await Task.Delay(1000);
+                            }
+                            clickType.Enabled = true;
+                            delayVal.ReadOnly = false;
+                            UpdateBtnStartText();
+                        });
+                    }
+                    else
+                    {
+                        MessageBox.Show("鼠标点击间隔，必须是等于或大于0的整数", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    running = false;
+                }
+            };
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            SetProcessDPIAware();
+            Application.EnableVisualStyles();
+            Application.Run(new MouseClickTool());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# MouseClickTool
+
+[![Windows .exe program](https://img.shields.io/badge/windows-.exe-0078D4?logo=windows)](https://ru.wikipedia.org/wiki/.EXE)
+
+简单好用的鼠标连点器，因为玩游戏需要连点，在网上找了好多连点器，有的居然还要收费...
+
+一向穷苦的我感觉为此花钱很不值得，索性自己写了个。。。
+
+最新版会记忆配置，支持设置热键，优化了性能
+
+## 下载
+
+[本地下载](https://github.com/lalakii/MouseClickTool/releases) | [123 云盘](https://www.123pan.com/s/jE3Sjv-IWExd.html) | [蓝奏云](https://a01.lanzout.com/b0hc64t7g) **密码：d59m**
+
+## 使用帮助
+
+如何退出？如果鼠标点击频率过快无法停下来，请先让程序窗口到前台（ALT+TAB），然后按 ALT+F4 关闭程序。
+
+如果有软件无法被点击，尝试以管理员身份运行此软件。
+
+设置热键时留意是否会和其他程序冲突。
+
+如果需要测试连点器速度，我知道这些网站：[点击速度测试 10 秒](https://cps-check.com/cn/) | [CPS 測試 (CPS Test) 手速測試 - 測手速](https://cpstest.org/zh/) | [CPS 测试 - 鼠标点击速度测试](https://www.arealme.com/click-speed-test/cn/)
+
+## 界面预览
+
+<img src="https://fastly.jsdelivr.net/gh/lalakii/MouseClickTool/MouseClickTool.jpg" width="350"/>
+
+## By lalaki.cn
