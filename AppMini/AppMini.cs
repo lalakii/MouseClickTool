2ba89c9f0f6bd0bc562c400753545bda8599f9f8
lalakii <i@lalaki.cn>
2025-12-19 14:39:32 +0800

    :memo: v2.9.2.0 - Add timestamps to cache filename.

--- AppMini/AppMini.cs
+++ AppMini/AppMini.cs
@@ -8,20 +8,20 @@ using System.Reflection;
 
 // main.
 var a = Environment.Is64BitProcess ? "x64" : "x86";
-var p = Path.Combine(Path.GetTempPath(), $"MouseClickTool_{Assembly.GetExecutingAssembly().GetName().Version}_{a}.dll");
-using (var f = File.Open(p, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+var p = Path.Combine(Path.GetTempPath(), $"MouseClickTool_{DateTime.Now:yyyy-MM}_{a}.dll");
+try
 {
-    try
+    using (var f = File.Open(p, FileMode.OpenOrCreate, FileAccess.Write))
     {
         if (f.Length == 0L)
         {
             new GZipStream(new System.Net.WebClient().OpenRead($"https://fastly.jsdelivr.net/gh/lalakii/MouseClickTool/App/{a}.GZ"), CompressionMode.Decompress).CopyTo(f);
         }
     }
-    catch
-    {
-        f.SetLength(0L);
-    }
-}
 
-Activator.CreateInstanceFrom(p, "MouseClickTool");
+    Activator.CreateInstanceFrom(p, "MouseClickTool");
+}
+catch
+{
+    File.Delete(p);
+}
