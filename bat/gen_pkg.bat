c68425a32675a5c053ee56a4c7e4c0ac7846269d
lalakii <i@lalaki.cn>
2025-12-19 19:08:19 +0800

    :memo:v2.9.2.0 - add gen_pkg.bat

---
+++ bat/gen_pkg.bat
@@ -0,0 +1,17 @@
+@echo off
+set procName=7za.exe
+%procName% > NUL 2>&1 &
+if "%ERRORLEVEL%" NEQ "0" (
+    echo 7za.exe not found. Please download it from https://7-zip.org/download.html and add it to your PATH environment variable.
+    exit -1
+)
+set projectName=App
+if EXIST "bin\dll\net462\MouseClickTool.dll" (
+    del .\%projectName%\x86.GZ /Q > NUL 2>&1
+    del .\%projectName%\x64.GZ /Q > NUL 2>&1
+    cd .\bin\dll\net462\ 
+    %procName% a -tgzip -mx9 -mtm- ..\..\..\%projectName%\x86.GZ MouseClickTool.dll > NUL 2>&1
+    copy  ..\..\..\%projectName%\x86.GZ  ..\..\..\%projectName%\x64.GZ > NUL 2>&1
+) else (
+   exit 0
+)
