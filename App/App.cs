b3ae6924befe70edf6d00f1bf14392dc142f1640
lalakii <i@lalaki.cn>
2025-12-22 15:57:31 +0800

    :memo: Load DLL from MemoryStream

--- App/App.cs
+++ App/App.cs
@@ -2,10 +2,6 @@
 using System.Reflection;
 
 // main.
-var p = Path.Combine(Path.GetTempPath(), $"{Path.GetRandomFileName()}.dll");
-using (var f = File.Create(p))
-{
-    new GZipStream(Assembly.GetEntryAssembly().GetManifestResourceStream("App.x86.GZ"), CompressionMode.Decompress).CopyTo(f);
-}
-
-Activator.CreateInstanceFrom(p, "MouseClickTool");
+using MemoryStream m = new();
+new GZipStream(Assembly.GetEntryAssembly().GetManifestResourceStream("App.x86.GZ"), CompressionMode.Decompress).CopyTo(m);
+Assembly.Load(m.ToArray()).CreateInstance("MouseClickTool");
